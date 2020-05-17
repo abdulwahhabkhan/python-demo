c28c220bed651833d1aa4546533d049134b49acc
Abdul Wahhab Khan <abdul.webequator.com>
2020-05-17 10:32:14 +0100

    happy new year

--- src/app.py
+++ src/app.py
@@ -1,13 +1,24 @@
-from flask import Flask
+import datetime
+
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "hello world!!!"
+    return render_template("index.html")
 
 
 @app.route("/<string:name>")
 def hello(name):
-    return f"hello, {name}"
+    name = name.capitalize()
+    return render_template("hello.html", name=name)
+
+
+@app.route("/isitnewyear")
+def new_year():
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    return render_template("new-year.html", new_year=new_year)
